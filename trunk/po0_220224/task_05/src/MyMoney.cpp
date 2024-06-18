9b796ae2cfb63933c3d4d1f7b5ccfe8b57e50799
ViktorKhvedorets <128993471+ViktorKhvedorets@users.noreply.github.com>
2024-06-18 15:46:59 +0300

    edit mymoney

--- trunk/po0_220224/task_05/src/MyMoney.cpp
+++ trunk/po0_220224/task_05/src/MyMoney.cpp
@@ -1,7 +1,61 @@
 #include "Money.h"
-#include <iostream>
-#include <iomanip>
+#include <cmath>
+#include <memory>
 
+// Конструктор, инициализирующий объект Money из вещественного числа
+Money::Money(double amount)
+    : rubles(static_cast<long>(amount)),
+      kopeck(static_cast<int>(std::round((amount - std::floor(amount)) * 100)))
+{
+    // Если количество копеек больше или равно 100, добавляем их к рублям
+    normalize();
+}
+
+
+
+
+// Оператор сложения двух объектов Money
+Money Money::operator+(const Money& other) const
+{
+    Money sum;
+    sum.rubles = rubles + other.rubles;
+    sum.kopeck = kopeck + other.kopeck;
+
+    // Нормализуем результат
+    sum.normalize();
+
+    return sum;
+}
+Money Money::operator-(const Money& a) const
+{
+    auto diff = std::make_unique<Money>();
+    long RDiff = this->GetR() - a.GetR();
+    int KDiff = this->GetK() - a.GetK();
+    if (KDiff < 0)
+    {
+        RDiff--;
+        KDiff += 100;
+    }
+    diff->SetK(KDiff);
+    diff->SetR(RDiff);
+    return *diff.get();
+}
+Money Money::operator/(const int a) const
+{
+    long totalKopecks = rubles * 100 + kopeck;
+    totalKopecks /= a;
+    long newRubles = totalKopecks / 100;
+    int newKopecks = totalKopecks % 100;
+    return Money(newRubles + newKopecks / 100.0);
+}
+
+// Оператор сравнения на равенство двух объектов Money
+bool Money::operator==(const Money& other) const
+{
+    return rubles == other.rubles && kopeck == other.kopeck;
+}
+
+// Дружественный оператор вывода объекта Money
 std::ostream& operator<<(std::ostream& out, const Money& a)
 {
     out << a.GetR() << ',' << std::setw(2) << std::setfill('0') << a.GetK();
@@ -17,3 +71,4 @@ std::istream& operator>>(std::istream& in, Money& a)
     a.normalize();
     return in;
 }
+
