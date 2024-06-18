3efa6cb9cf092885eb8c998b806d3bcbb62e96a0
ViktorKhvedorets <128993471+ViktorKhvedorets@users.noreply.github.com>
2024-06-18 19:38:33 +0300

    Update Money.h

--- trunk/po0_220224/task_05/src/Money.h
+++ trunk/po0_220224/task_05/src/Money.h
@@ -8,13 +8,12 @@ class Coins
 public:
     Coins() = default;
     explicit Coins(const double a);
-    Coins(long r, int k); // Constructor to accept rubles and kopecks directly
+    Coins(long int r, int k); // Constructor to accept rubles and kopecks directly
     Coins(const Coins& other) = default;
     ~Coins() = default;
     Coins& operator=(const Coins& a) = default;
     bool operator==(const Coins& a) const;
     auto operator<=>(const Coins& a) const = default;
-    Coins operator+(const Coins& a) const;
 
     friend std::ostream& operator << (std::ostream& out, const Coins& a)
     {
@@ -35,6 +34,18 @@ public:
     inline long GetR() const { return rubles; };
     inline void SetK(const int k) { kopeck = k; };
     inline void SetR(const long r) { rubles = r; };
+
+    Coins operator+(const Coins& a) const
+    {
+        long totalRubles = this->rubles + a.rubles;
+        int totalKopecks = this->kopeck + a.kopeck;
+        if (totalKopecks >= 100)
+        {
+            totalRubles++;
+            totalKopecks -= 100;
+        }
+        return Coins(totalRubles, totalKopecks);
+    }
 
 private:
     long rubles = 0;
